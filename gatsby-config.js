d418786ad1c4ea85350e1899a75caae08afa96f2
Joshua Anderson <joshuatanderson1@gmail.com>
2021-01-23 15:49:34 -0600

    fix: remove gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,32 +7,32 @@ const contentfulConfig = {
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 }
 
-const googleAnalyticsConfig = {
-  // The property ID; the tracking code won't be generated without it
-  trackingId: "G-TLQTZLQPB5",
-  // Defines where to place the tracking script - `true` in the head and `false` in the body
-  head: true,
-  // Setting this parameter is optional
-  anonymize: true,
-  // Setting this parameter is also optional
-  respectDNT: true,
-  // Avoids sending pageview hits from custom paths
-  exclude: ["/preview/**", "/do-not-track/me/too/"],
-  // Delays sending pageview hits on route update (in milliseconds)
-  pageTransitionDelay: 0,
-  // Enables Google Optimize using your container Id
-  // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-  // Enables Google Optimize Experiment ID
-  // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-  // Set Variation ID. 0 for original 1,2,3....
-  // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-  // Defers execution of google analytics script after page load
-  defer: false,
-  // Any additional optional fields
-  sampleRate: 5,
-  siteSpeedSampleRate: 10,
-  cookieDomain: "noeasyquestions.com",
-},
+// const googleAnalyticsConfig = {
+//   // The property ID; the tracking code won't be generated without it
+//   trackingId: "G-TLQTZLQPB5",
+//   // Defines where to place the tracking script - `true` in the head and `false` in the body
+//   head: true,
+//   // Setting this parameter is optional
+//   anonymize: true,
+//   // Setting this parameter is also optional
+//   respectDNT: true,
+//   // Avoids sending pageview hits from custom paths
+//   exclude: ["/preview/**", "/do-not-track/me/too/"],
+//   // Delays sending pageview hits on route update (in milliseconds)
+//   pageTransitionDelay: 0,
+//   // Enables Google Optimize using your container Id
+//   // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+//   // Enables Google Optimize Experiment ID
+//   // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+//   // Set Variation ID. 0 for original 1,2,3....
+//   // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+//   // Defers execution of google analytics script after page load
+//   defer: false,
+//   // Any additional optional fields
+//   sampleRate: 5,
+//   siteSpeedSampleRate: 10,
+//   cookieDomain: "noeasyquestions.com",
+// },
 
 // if you want to use the preview API please define
 // CONTENTFUL_HOST in your environment config
@@ -64,9 +64,9 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: googleAnalyticsConfig
-    }
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: googleAnalyticsConfig
+    // }
   ],
 }
