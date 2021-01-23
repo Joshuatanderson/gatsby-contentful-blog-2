876f64230b001d535d7826b2cd26bd8fc9770923
Joshua Anderson <joshuatanderson1@gmail.com>
2021-01-23 15:46:44 -0600

    fix: flip head to true in g-tag config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ const googleAnalyticsConfig = {
   // The property ID; the tracking code won't be generated without it
   trackingId: "G-TLQTZLQPB5",
   // Defines where to place the tracking script - `true` in the head and `false` in the body
-  head: false,
+  head: true,
   // Setting this parameter is optional
   anonymize: true,
   // Setting this parameter is also optional
